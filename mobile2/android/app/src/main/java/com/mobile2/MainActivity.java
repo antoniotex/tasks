440b70b9b1c0988386cdca5b72a4020601029290
Antonio Carlos <acm.skt@gmail.com>
2020-06-08 19:34:30 -0300

    Altera nome da aplicação

--- mobile2/android/app/src/main/java/com/mobile2/MainActivity.java
+++ mobile2/android/app/src/main/java/com/mobile2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mobile2;
+package com.Tasks;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mobile2";
+    return "Tasks";
   }
 }
