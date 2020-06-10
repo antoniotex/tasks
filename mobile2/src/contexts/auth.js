c286b85a9e2f287e72b19d92ebe8e1ee2000ddb3
Antonio Carlos <acm.skt@gmail.com>
2020-06-10 15:38:06 -0300

    Add async storage community / atualiza body-parser

--- mobile2/src/contexts/auth.js
+++ mobile2/src/contexts/auth.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { AsyncStorage, ToastAndroid } from 'react-native';
+import { ToastAndroid } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import api, * as auth from '../services/api';
 
 const AuthContext = createContext({ signed: false, user: {} });
--- mobile2/src/contexts/posters.js
+++ mobile2/src/contexts/posters.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { AsyncStorage } from 'react-native';
+// import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import api, * as auth from '../services/api';
 
 const PosterContext = createContext({ posters: [] });
